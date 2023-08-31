f8d0d34883a113fef92a48bc6360a0b099768e1f
Danil <danil.komyshev777@gmail.com>
2023-08-31 22:47:52 +0300

    [UPD] README.MD and requirements.txt

--- settings.py
+++ settings.py
@@ -1,15 +1,13 @@
 import pathlib
-
-from pydantic import BaseSettings, conint, constr
-from pydantic.validators import IPv4Address
+from pydantic import conint, constr
 
 
-class Settings(BaseSettings):
+class Settings:
     DEBUG: bool = True
 
     APP_TITLE: constr(min_length=1, max_length=255) = 'CKD_API'
     APP_VERSION: constr(min_length=1, max_length=15) = '1'
-    APP_HOST: constr(min_length=1, max_length=15) = str(IPv4Address('127.0.0.1' if DEBUG else '0.0.0.0'))
+    APP_HOST: constr(min_length=1, max_length=15) = str('127.0.0.1' if DEBUG else '0.0.0.0')
     APP_PORT: conint(ge=0) = 5000
     APP_PATH: constr(min_length=1, max_length=255) = str(pathlib.Path(__file__).parent.resolve())
 
